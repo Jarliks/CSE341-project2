ed481bb0310141452cd88b24852fca5df412c423
Jarliks <alecmateski@gmail.com>
2023-10-01 22:52:58 -0700

    added 'else' to requests

--- controllers/_collection2.js
+++ controllers/_collection2.js
@@ -5,15 +5,16 @@ const getSingle = async (req, res) => {
     //#swagger.tags=['_collection2']
     if (!ObjectId.isValid(req.params.id)) {
         res.status(400).json('Must be a valid ID to retrieve data.');
+    } else {
+        const _collection2Id = new ObjectId(req.params.id);
+        mongodb.getDb().db().collection('_collection2').find({ _id: _collection2Id}).toArray((err, lists) => {
+                if (err) {
+                    res.status(400).json({ message: err});
+                }
+                res.setHeader('contentType', 'application/json');
+                res.status(200).json(lists);
+            });
     }
-    const _collection2Id = new ObjectId(req.params.id);
-    mongodb.getDb().db().collection('_collection2').find({ _id: _collection2Id}).toArray((err, lists) => {
-            if (err) {
-                res.status(400).json({ message: err});
-            }
-            res.setHeader('contentType', 'application/json');
-            res.status(200).json(lists);
-        });
 };
 
 const create_collection2 = async (req, res) => {
@@ -35,33 +36,35 @@ const update_collection2 = async (req, res) => {
     //#swagger.tags=['_collection2']
     if (!ObjectId.isValid(req.params.id)) {
         res.status(400).json('Must be a valid ID to update data.');
-    }
-    const _collection2Id = new ObjectId(req.params.id);
-    const _collection2data = {
-        username: req.body.username,
-        question: req.body.question,
-        answer: req.body.answer
-    };
-    const response = await mongodb.getDatabase().db().collection('_collection2').replaceOne({ _id: _collection2Id }, _collection2data);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
     } else {
-        res.status(500).json(response.error || 'Some error occured while updating the data.');
-    };
+        const _collection2Id = new ObjectId(req.params.id);
+        const _collection2data = {
+            username: req.body.username,
+            question: req.body.question,
+            answer: req.body.answer
+        };
+        const response = await mongodb.getDatabase().db().collection('_collection2').replaceOne({ _id: _collection2Id }, _collection2data);
+        if (response.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occured while updating the data.');
+        };
+    }
 };
 
 const delete_collection2 = async (req, res) => {
     //#swagger.tags=['_collection2']
     if (!ObjectId.isValid(req.params.id)) {
         res.status(400).json('Must be a valid ID to delete data.');
-    }
-    const _collection2Id = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('_collection2').deleteOne({ _id: _collection2Id });
-    if (response.deletedCount > 0) {
-        res.status(204).send();
     } else {
-        res.status(500).json(response.error || 'Some error occured while updating the data.');
-    };
+        const _collection2Id = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection('_collection2').deleteOne({ _id: _collection2Id });
+        if (response.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occured while updating the data.');
+        };
+    }
 };
 
 module.exports = {
